3e9b9a7534117163a3943d6e14d4da816a940a49
Han-D-Peter <57760806+Han-D-Peter@users.noreply.github.com>
2021-10-12 23:24:48 +0900

    styled-components

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,3 +1,33 @@
-const Login = () => <h1>Login</h1>;
+import { useState } from "react";
+import styled, { css } from "styled-components";
+import { darkModeVar, isLoggedInVar } from "../apollo";
+
+const Title = styled.h1`
+  color: ${props => props.theme.fontColor};
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+  ${props =>
+    props.potato
+      ? css`
+          font-size: 49px;
+        `
+      : css`
+          text-decoration: underline;
+        `}
+`;
+
+const Container = styled.div`
+  background-color: ${props => props.theme.bgColor};
+`;
+
+const Login = () => {
+  return (
+    <Container>
+      <Title>Login</Title>
+      <button onClick={() => darkModeVar(true)}>To dark</button>
+      <button onClick={() => darkModeVar(false)}>To light</button>
+    </Container>
+  );
+};
 
 export default Login;
